b2e9a32216d72a78227c95817966b5e28dd34b22
Nicholas Hellmers <nihe1661@colorado.edu>
2023-09-04 19:26:40 -0600

    Defining some stuff

--- main.go
+++ main.go
@@ -5,7 +5,18 @@ package main
 // to schedule tasks. It takes in a set of tasks, and prints out the
 // shortest time to complete all tasks.
 
+// The graph is a directed acyclic graph, the edges are weighted by the
+// time between tasks, and the nodes are the tasks themselves.
+
+type Graph struct {
+	Nodes []Node `json:"Nodes"`
+	Edges [][2]Node
+}
+
 type Node struct {
-	Name string
-	Time int
+	Name     string `json:"Name"`
+	Id       int    `json:"Id"`
+	Day      int    `json:"Day"`
+	Time     int    `json:"Time"`
+	Priority int    `json:"Priority"`
 }
